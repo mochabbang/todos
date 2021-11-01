bdb2e7663c418cdb1d90a5c3b4d9d0337e231574
Sungmin Lee <sungmin9036@gmail.com>
2021-11-01 22:20:29 +0900

    ReactJs 투드리스트 목록, 신규생성 란 추가-21.11.01

--- todos_frontend/src/components/TodoList.js
+++ todos_frontend/src/components/TodoList.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import TodoItem from './TodoItem';
 
 const TodoListBlock = styled.div`
     flex: 1;
     padding: 20px 32px 48px;
     overflow-y: auto;
-    background: grey;
 `;
 
 function TodoList() {
     return (
         <TodoListBlock>
-            TodoList
+            <TodoItem text="프로젝트 생성하기" done={true} />
+            <TodoItem text="컴포넌트 스타일링 하기" done={true} />
+            <TodoItem text="Context 만들기" done={false} />
+            <TodoItem text="기능 구현하기" done={false} />
         </TodoListBlock>
     );
 }
