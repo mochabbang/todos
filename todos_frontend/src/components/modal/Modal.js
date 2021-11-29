b507e36ac86de3ddf685222c040cc2d6530a3ecb
Sungmin Lee <sungmin9036@gmail.com>
2021-11-29 23:07:43 +0900

    Modal Styled컴포넌트로 변경작업-21.11.29

--- todos_frontend/src/components/modal/Modal.js
+++ todos_frontend/src/components/modal/Modal.js
@@ -2,6 +2,26 @@ import React from 'react';
 import '../modal/modal.css';
 import styled, { css, keyframes } from 'styled-components';
 
+const modalShow = keyframes`
+    from {
+        opacity: 0;
+        margin-top: -50px;
+    }
+    to {
+        opacity: 1;
+        margin-top: 0;
+    }
+`;
+
+const modalBgShow = keyframes`
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+`;
+
 const ModalBackGround = styled.div`
     display: ${ ({open}) => open ? 'none' : 'flex' };
     position: fixed;
@@ -15,7 +35,7 @@ const ModalBackGround = styled.div`
         ({open}) => open && 
         css`
             align-items: center;
-            animation: modal-bg-show .3s;
+            animation: ${ modalBgShow } .3s;
         `
     }
 `;
@@ -26,10 +46,43 @@ const ModalSection = styled.section`
     margin: 0 auto;
     border-radius: .3rem;
     background-color: #fff;
-    animation: modal-show .3s;
+    animation: ${ modalShow } .3s;
     overflow: hidden;
+    
 `;
 
+const ModalHeader = styled.header``;
+
+const ModalMain = styled.main`
+    padding: 16px;
+    border-bottom: 1px solid #dee2e6;
+    border-top: 1px solid #dee2e6;
+`;
+
+const ModalFooter = styled.footer`
+    padding: 12px 16px;
+    text-align: right;
+`;
+
+const ModalHeaderButton = styled.button`
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    width: 30px;
+    font-size: 21px;
+    font-weight: 700;
+    text-align: center;
+    color: #999;
+    background-color: transparent;
+`;
+
+const ModalFooterButton = styled.button`
+    padding: 6px 12px;
+    color: #fff;
+    background-color: #6c757d;
+    border-radius: 5px;
+    font-size: 13px;
+`;
 
 const Modal = (props) => {
     // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
@@ -37,24 +90,42 @@ const Modal = (props) => {
 
     return (
         // 모달이 열릴 때 openModal 클래스가 생성된다.
-        <div className={ open ? 'openModal modal' : 'modal'}>
+        // <div className={ open ? 'openModal modal' : 'modal'}>
+        //     {
+        //         open ? (
+        //             <section>
+        //                 <header>
+        //                     <button className="close" onClick={close}>&times;</button>
+        //                 </header>
+        //                 <main>
+        //                     {props.children}
+        //                 </main>
+        //                 <footer>
+        //                     <button className="close" onClick={onSubmit}> save </button>
+        //                     &nbsp;<button className="close" onClick={close}> close </button>
+        //                 </footer>
+        //             </section>
+        //         ) : null
+        //     }
+        // </div>
+        <ModalBackGround>
             {
                 open ? (
-                    <section>
-                        <header>
-                            <button className="close" onClick={close}>&times;</button>
-                        </header>
-                        <main>
-                            {props.children}
-                        </main>
-                        <footer>
-                            <button className="close" onClick={onSubmit}> save </button>
-                            &nbsp;<button className="close" onClick={close}> close </button>
-                        </footer>
-                    </section>
+                    <ModalSection>
+                        <ModalHeader>
+                            <ModalHeaderButton onClick={close}>&times;</ModalHeaderButton>                            
+                        </ModalHeader>
+                        <ModalMain>
+                                {props.children}
+                        </ModalMain>
+                        <ModalFooter>
+                            <ModalFooterButton onClick={onSubmit}> save </ModalFooterButton>
+                            &nbsp;<ModalFooterButton onCLick={close}> close </ModalFooterButton>
+                        </ModalFooter>
+                    </ModalSection>
                 ) : null
             }
-        </div>
+        </ModalBackGround>
     )
 }
 
